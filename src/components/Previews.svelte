ae745e9d64388665adaeeaf4c8231287b4927658
Kevin Daněk <heindrich73@gmail.com>
2023-07-08 16:18:29 +0200

    feat: Nastavitelný layout

--- src/components/Previews.svelte
+++ src/components/Previews.svelte
@@ -1,11 +1,22 @@
 <script>
     import BigPicture from "./BigPicture.svelte";
+    import { layout } from "../stores/Layout";
 
+    let style = "";
+    let length = 0;
+
+    $: {
+        style = `
+            grid-template-rows: repeat(${$layout.rows}, 1fr);
+            grid-template-columns: repeat(${$layout.columns}, 1fr);
+        `;
+
+        length = $layout.rows * $layout.columns;
+    }
 </script>
 
-<section class="grid grid-cols-2 grid-rows-2 h-full w-full p-2 gap-2">
-    <BigPicture/>
-    <BigPicture/>
-    <BigPicture/>
-    <BigPicture/>
+<section class="grid h-full w-full p-2 gap-2" {style}>
+    {#each { length } as _}
+        <BigPicture />
+    {/each}
 </section>
